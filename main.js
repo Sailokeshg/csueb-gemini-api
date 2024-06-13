83d97484a7f96fc735aed0edb7e95a37f473c620
sailokeshreddyg <sailokeshreddyg@gmail.com>
2024-06-13 21:09:48 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import MarkdownIt from 'markdown-it';
+import { maybeShowApiKeyBanner } from './gemini-api-banner';
+import './style.css';
+
+let API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+
+let form = document.querySelector('form');
+let promptInput = document.querySelector('input[name="prompt"]');
+let output = document.querySelector('.output');
+
+form.onsubmit = async (ev) => {
+  ev.preventDefault();
+  output.textContent = 'Generating...';
+
+  try {
+    let contents = [
+      {
+        role: 'user',
+        parts: [
+          { text: promptInput.value }
+        ]
+      }
+    ];
+
+    // Call the text-bison-002 model, and get a stream of results
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({
+      // model: "gemini-1.0-pro", 
+      model:'gemini-1.5-pro',
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+      ],
+    });
+
+    const result = await model.generateContentStream({ contents });
+
+    // Read from the stream and interpret the output as markdown
+    let buffer = [];
+    let md = new MarkdownIt();
+    for await (let response of result.stream) {
+      buffer.push(response.text());
+      output.innerHTML = md.render(buffer.join(''));
+    }
+  } catch (e) {
+    output.innerHTML += '<hr>' + e;
+  }
+};
